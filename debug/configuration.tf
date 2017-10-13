336968b570943c5f9aa037505297540b5992acdc
Ron Kurr <kurr@kurron.org>
2017-10-13 16:32:19 -0400

    Use only one subnet

--- debug/configuration.tf
+++ debug/configuration.tf
@@ -9,6 +9,6 @@ environment = "development"
 freetext    = "No notes at this time."
 
 cidr_range         = "10.0.0.0/16"
-public_subnets     = ["10.0.2.0/24","10.0.4.0/24","10.0.6.0/24","10.0.8.0/24","10.0.10.0/24","10.0.12.0/24","10.0.14.0/24"]
-private_subnets    = []
-populate_all_zones = "true"
+private_subnets    = ["10.0.1.0/24"]
+public_subnets     = ["10.0.2.0/24"]
+populate_all_zones = "false"
