ab340a10d0df76d3fdb7ff50d40b1869888dc837
Ron Kurr <kurr@kurron.org>
2017-10-15 11:53:08 -0400

    Only create public subnets

--- debug/plan.tf
+++ debug/plan.tf
@@ -9,6 +9,6 @@ environment = "development"
 freetext    = "No notes at this time."
 
 cidr_range         = "10.0.0.0/16"
-private_subnets    = ["10.0.1.0/24"]
-public_subnets     = ["10.0.2.0/24"]
-populate_all_zones = "false"
+private_subnets    = []
+public_subnets     = ["10.0.2.0/24","10.0.4.0/24","10.0.6.0/24"]
+populate_all_zones = "true"
